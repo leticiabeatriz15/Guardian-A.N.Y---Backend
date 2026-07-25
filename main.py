d8e750139939e9e66e934eecb74c6da22ba4b23e
Leticia Beatriz <leticiabeatriz1508@gmail.com>
2026-07-25 09:26:51 -0300

    mudando maneira de acesso, utilizando cookies

--- main.py
+++ main.py
@@ -44,14 +44,19 @@ def extrair_metricas_e_titulo(url_video: str):
     print(f"\n[1/4] Extraindo stream via yt-dlp: {url_norm}")
     
     ydl_opts = {
-        'format': 'best[ext=mp4]/best',
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best',        
+        'cookiefile': 'cookies.txt',
         'quiet': True,
-        'cookiefile': 'cookies.txt', 
+        'no_warnings': True,        
         'extractor_args': {
             'youtube': {
-                'player_client': ['android', 'ios'],
+                'player_client': ['web', 'web_creator', 'tv_embedded']
             }
         },
+        'http_headers': {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)    Chrome/122.0.0.0 Safari/537.36',
+                    'Accept-Language': 'en-US,en;q=0.9',
+                }
     }
 
     try:
