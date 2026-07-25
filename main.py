4d798b297a4348e739136b7c69fbe02bfb7c5322
Leticia Beatriz <leticiabeatriz1508@gmail.com>
2026-07-25 09:14:02 -0300

    Ajustando

--- main.py
+++ main.py
@@ -44,18 +44,19 @@ def extrair_metricas_e_titulo(url_video: str):
     print(f"\n[1/4] Extraindo stream via yt-dlp: {url_norm}")
     
     ydl_opts = {
-        'ignoreerrors': True,
-        'quiet': True,
-        'format': 'worst[ext=mp4]/best[height<=360][ext=mp4]/worst/best',
-        'extractor_args': {
-            'youtube': {
-                'player_client': ['ios', 'android', 'tv', 'mweb'],
-                'player_skip': ['webpage', 'configs'],
-            }
-        },
-        'geo_bypass': True,
-        'user_agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
+    'format': 'best',
+    'quiet': True,
+    'no_warnings': True,
+    'extractor_args': {
+        'youtube': {
+            'player_client': ['ios', 'android', 'mweb'], 
+        }
+    },
+    'http_headers': {
+        'User-Agent': 'com.google.android.youtube/19.09.37 (Linux; U; Android 11; US) gzip',
+        'Accept-Language': 'en-US,en;q=0.9',
     }
+}
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
